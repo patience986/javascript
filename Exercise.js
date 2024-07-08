976dcf4561f4caddbcdbb7eb7980cb64a04bca4c
patience986 <nalweisopatience@gmail.com>
2024-07-08 12:37:16 +0300

    my work

---
+++ Exercise.js
@@ -0,0 +1,18 @@
+
+
+let dealsIn = ["beans",
+    "grain maize","cowpeas","g-nuts","rice","soybeans"
+]
+//stings
+let nameOfTheProduce = "maize"
+let typeOfProduce = ""
+let timeOfProduce =""
+let dealerName ="patience"
+
+let weightOfMaize = 1000
+
+let isExeceeding = weightis
+
+let produce={
+    
+}
---
+++ conditions.js
@@ -0,0 +1,23 @@
+//condition statements are for making conditions
+//if
+//if else
+//if else if else
+//switch
+//ternary operator
+
+// if(condition)
+//      {
+//        function
+
+let number = 5;
+if (number > 0) {
+    console.log(`${number} is a positive number`);
+}
+
+let isShinning=false
+if(isShinning)
+{
+    console.log( `${comment}carry sun glasses`)
+}
+else{console.log("no need for sun glasses")}
+if 
